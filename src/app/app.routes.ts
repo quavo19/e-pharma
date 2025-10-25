4fcaad5c7142f6ecb3748c3cba3f4f2bcad0ece2
Donald Akite <donaldakite27@gmail.com>
2025-10-25 20:34:09 +0000

    Add routing configuration for login, documentation, and 404 pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'docs',
+    loadComponent: () =>
+      import('./pages/docs/docs.component').then((m) => m.DocsComponent),
+  },
+  {
+    path: '404',
+    loadComponent: () =>
+      import('./pages/not-found/not-found.component').then(
+        (m) => m.NotFoundComponent
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: '/404',
+  },
+];
