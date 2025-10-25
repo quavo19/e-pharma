6ff92b6ad17130c089afe6f46870a429a6b75807
Donald Akite <donaldakite27@gmail.com>
2025-10-25 23:45:08 +0000

    Add unit tests for LoginFormComponent and integrate it into LoginComponent with icon support

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { LoginFormComponent } from '../../components/login-form/login-form.component';
+import { Shield, WifiOff, LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [LoginFormComponent, LucideAngularModule],
   templateUrl: './login.component.html',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  protected readonly icons = { Shield, WifiOff };
+}
