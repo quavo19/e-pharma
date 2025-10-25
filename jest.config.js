6ddf581e4e79902d39b0be1ac0a783a570e57ec7
Donald Akite <donaldakite27@gmail.com>
2025-10-25 02:57:02 +0000

    Refactor Jest configuration and update tsconfig.spec.json; add InputComponent template for enhanced form handling

--- jest.config.js
+++ jest.config.js
@@ -1,31 +1,27 @@
 module.exports = {
-  preset: 'jest-preset-angular',
-  setupFilesAfterEnv: ['<rootDir>/src/setup-jest.ts'],
-  testEnvironment: 'jsdom',
+  preset: "jest-preset-angular",
+  setupFilesAfterEnv: ["<rootDir>/src/setup-jest.ts"],
+  testEnvironment: "jsdom",
   transform: {
-    '^.+\\.(ts|mjs|js|html)$': [
-      'jest-preset-angular',
+    "^.+\\.(ts|mjs|js|html)$": [
+      "jest-preset-angular",
       {
-        tsconfig: '<rootDir>/tsconfig.spec.json',
-        stringifyContentPathRegex: '\\.(html|svg)$',
+        tsconfig: "<rootDir>/tsconfig.spec.json",
+        stringifyContentPathRegex: "\\.(html|svg)$",
       },
     ],
   },
-  transformIgnorePatterns: [
-    'node_modules/(?!.*\\.mjs$)',
-  ],
+  transformIgnorePatterns: ["node_modules/(?!.*\\.mjs$)"],
   collectCoverageFrom: [
-    'src/**/*.ts',
-    '!src/**/*.spec.ts',
-    '!src/**/*.d.ts',
-    '!src/main.ts',
-    '!src/polyfills.ts',
+    "src/**/*.ts",
+    "!src/**/*.spec.ts",
+    "!src/**/*.d.ts",
+    "!src/main.ts",
+    "!src/polyfills.ts",
   ],
-  coverageDirectory: 'coverage',
-  coverageReporters: ['text', 'lcov', 'html'],
-  testMatch: [
-    '<rootDir>/src/**/*.spec.ts',
-  ],
-  moduleFileExtensions: ['ts', 'html', 'js', 'json', 'mjs'],
-  resolver: 'jest-preset-angular/build/resolvers/ng-jest-resolver.js',
+  coverageDirectory: "coverage",
+  coverageReporters: ["text", "lcov", "html"],
+  testMatch: ["<rootDir>/src/**/*.spec.ts"],
+  moduleFileExtensions: ["ts", "html", "js", "json", "mjs"],
+  resolver: "jest-preset-angular/build/resolvers/ng-jest-resolver.js",
 };
