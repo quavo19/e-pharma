0afe06e26ffa2c9b660d7313acf1a32c7013c6ed
Bismark Yamoah <81543123+KBismark@users.noreply.github.com>
2025-10-25 02:51:35 +0000

    Add InputComponent and FormControl to app.component.ts for email input handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { InputComponent } from './components/input/input.component';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, InputComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
+  emailControl = new FormControl('');
   title = 'pms';
 }
