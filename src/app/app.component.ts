3a1268f108620c22ccd911f39e80106dd394f15d
Donald Akite <donaldakite27@gmail.com>
2025-10-25 20:34:28 +0000

    Refactor app.component.ts to remove InputComponent and FormControl, simplifying the component structure. Update app.config.ts to include HTTP client with auth interceptor for improved API handling.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { InputComponent } from './components/input/input.component';
-import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, InputComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  emailControl = new FormControl('');
-  title = 'pms';
-}
+export class AppComponent {}
