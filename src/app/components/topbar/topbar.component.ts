7862903b5c468633e28d24940f644f766b946cec
Donald Akite <donaldakite27@gmail.com>
2025-10-29 20:01:01 +0000

    Refactor TopbarComponent by removing unused DatepickerComponent and FormsModule imports, and eliminate date selection logic. Streamline component structure for improved clarity and performance.

--- src/app/components/topbar/topbar.component.ts
+++ src/app/components/topbar/topbar.component.ts
@@ -1,29 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DatepickerComponent } from '../datepicker/datepicker.component';
-import { FormsModule } from '@angular/forms';
 import { LucideAngularModule, Bell } from 'lucide-angular';
 
 @Component({
   selector: 'app-topbar',
-  imports: [
-    CommonModule,
-    DatepickerComponent,
-    FormsModule,
-    LucideAngularModule,
-  ],
+  imports: [CommonModule, LucideAngularModule],
   templateUrl: './topbar.component.html',
 })
 export class TopbarComponent {
   public readonly icons = { Bell };
-  selectedDate: Date | null = new Date();
 
   onNotificationClick(): void {
     console.log('Notification clicked');
-  }
-
-  onDateChange(date: Date | null): void {
-    this.selectedDate = date;
-    console.log('Selected date:', date);
   }
 }
