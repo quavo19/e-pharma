1a60637b57437263914b298de7a3a064e52f3e3e
Donald Akite <donaldakite27@gmail.com>
2025-10-29 00:33:25 +0000

    Refactor dashboard component to include dashboard cards, top products, and monthly chart in the template

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { DashboardCardsComponent } from '../../components/dashboard-cards/dashboard-cards.component';
+import { TopProductsComponent } from '../../components/top-products/top-products.component';
+import { MonthlyChartComponent } from '../../components/monthly-chart/monthly-chart.component';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [
+    DashboardCardsComponent,
+    TopProductsComponent,
+    MonthlyChartComponent,
+  ],
   templateUrl: './dashboard.component.html',
 })
 export class DashboardComponent {}
