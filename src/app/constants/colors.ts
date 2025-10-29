382d8de8781ca3dfa862325ce3e28abb2c4fe7f4
Donald Akite <donaldakite27@gmail.com>
2025-10-29 00:32:46 +0000

    Add Datepicker component HTML structure and improve formatting for readability

---
+++ src/app/constants/colors.ts
@@ -0,0 +1,26 @@
+export const TOP_SALES_DATA = [
+  {
+    name: 'Henry Elorm Awunye',
+    sales: 320000,
+    displayValue: 'GH₵ 320,000',
+    percentage: 50,
+  },
+  {
+    name: 'Nana Adwoa Awuraba',
+    sales: 10000,
+    displayValue: 'GH₵ 10,000',
+    percentage: 25,
+  },
+  {
+    name: 'Lloyd Abaka',
+    sales: 4000,
+    displayValue: 'GH₵ 4,000',
+    percentage: 15,
+  },
+  {
+    name: 'Sandra Amanu Tenyson',
+    sales: 2000,
+    displayValue: 'GH₵ 2,000',
+    percentage: 10,
+  },
+] as const;
