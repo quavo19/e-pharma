ac080bae1cae1aae3b9ed3b8f76116bfad0aacd9
Donald Akite <donaldakite27@gmail.com>
2025-11-01 20:19:38 +0000

    Add DataTableComponent to create a reusable data table with dynamic columns and action menus. Implement header dropdowns for enhanced sorting and filtering options, improving user interaction and data management.

---
+++ src/app/components/data-table/data-table.component.ts
@@ -0,0 +1,39 @@
+import { Component, input, TemplateRef, ContentChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  TableHeaderDropdownComponent,
+  TableHeaderDropdownOption,
+} from '../table-header-dropdown/table-header-dropdown.component';
+import {
+  ActionMenuComponent,
+  ActionMenuItem,
+} from '../action-menu/action-menu.component';
+
+export interface TableColumn<T = any> {
+  key: string;
+  label: string;
+  headerDropdown?: {
+    options: TableHeaderDropdownOption[];
+    queryParamKey: string;
+    clearLabel?: string;
+    enableSearch?: boolean;
+    dropdownWidth?: string;
+  };
+  cellTemplate?: TemplateRef<{ $implicit: T; index: number }>;
+}
+
+@Component({
+  selector: 'app-data-table',
+  imports: [CommonModule, TableHeaderDropdownComponent, ActionMenuComponent],
+  templateUrl: './data-table.component.html',
+})
+export class DataTableComponent<T = any> {
+  columns = input.required<TableColumn<T>[]>();
+  data = input.required<T[]>();
+  getActionMenuItems = input<(item: T, index: number) => ActionMenuItem[]>();
+  trackBy = input<(index: number, item: T) => any>((index, item) => index);
+
+  getNestedValue(obj: any, path: string): any {
+    return path.split('.').reduce((current, prop) => current?.[prop], obj);
+  }
+}
