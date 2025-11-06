76ad9f982417cb52cca9bd83ab9c7f070e7a2513
Donald Akite <donaldakite27@gmail.com>
2025-11-06 12:39:17 +0000

    Add index file for Suppliers components to streamline exports. This includes exports for StarRatingComponent, SupplierReviewComponent, SupplierCard, and SupplierDetails, enhancing modularity and ease of access to supplier-related components.

---
+++ src/app/components/suppliers/index.ts
@@ -0,0 +1,4 @@
+export * from './star-rating/star-rating.component';
+export * from './supplier-review/supplier-review.component';
+export * from './supplier-card/supplier-card.component';
+export * from './supplier-details/supplier-details.component';
